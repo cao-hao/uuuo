e2a920516962c4b6b13cef5c0c7b07d6ee0c93a9
李新龙 <2194297577@qq.com>
2019-09-04 13:48:50 +0800

    增加了一个项目 增加了5/2的计算

---
+++ s201801260109/java1909/src/java1909/test01.java
@@ -0,0 +1,16 @@
+package java1909;
+
+import java.util.Scanner;
+
+public class test01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a / b);
+		
+		scanner.close();
+	}
+
+}
