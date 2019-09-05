de2e75f78ea297be4dbc6d0ed4766817a99d132c
黄帅琦 <1173750778@qq.com>
2019-09-05 10:51:08 +0800

    新增一个项目 计算5/2

---
+++ s201801260105/java190904/Test01.java
@@ -0,0 +1,14 @@
+package java1909;
+
+import java.util.Scanner;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a / b);
+		scanner.close();
+	}
+}
