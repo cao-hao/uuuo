504806c8a946bc0920bff28feb876aa253c1a1a4
黄帅琦 <1173750778@qq.com>
2019-09-16 11:29:36 +0800

    新增一个代码

---
+++ s201801260105/java190901/src/java19999.java
@@ -0,0 +1,16 @@
+
+public class java19999 {
+
+	public static void main(String[] args) {
+		
+	}
+	public static int fact(int n) {
+		if(n==0) {
+			return 1;
+		}
+		return n*fact(n-1);
+	}
+}
+
+
+
