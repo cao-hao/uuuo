ce9efd518c7913717510eb4410cde00a61c54dc6
766426 <2753664763@qq.com>
2019-09-16 11:22:50 +0800

    添加1-100整数和

---
+++ s201801260122/Java19090401/src/Test19091602.java
@@ -0,0 +1,12 @@
+
+public class Test19091602 {
+	public static void main(String[] args) {
+		int sum=0;
+		for (int i =1; i<=100; i+=2) {
+			sum +=i;
+		}
+		System.out.println("1-100之间的整数和为; "+sum);
+		}
+	}
+
+
