8509cbf9aae2c5a222300ff09f79df29103c4517
李新龙 <2194297577@qq.com>
2019-09-16 11:31:02 +0800

    增加text.19091602.java文件计算1-100的奇数和 增加text.19091603.java文件计算5的阶乘

---
+++ s201801260109/java1909/src/java1909/Text19091602.java
@@ -0,0 +1,12 @@
+package java1909;
+
+public class Text19091602 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i < 100; i+=2) {
+			
+			sum += i;
+		}
+		System.out.println("1-100之间的奇数和为："+sum);
+	}
+}
